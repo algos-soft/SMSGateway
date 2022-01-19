c2b5e162e7b62a36be22b121b40fcdaa04a9f665
algos-soft <algos12>
2022-01-19 19:11:19 +0100

    settingsActivity

--- app/src/main/java/it/algos/smsgateway/MainActivity.java
+++ app/src/main/java/it/algos/smsgateway/MainActivity.java
@@ -1,6 +1,7 @@
 package it.algos.smsgateway;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
@@ -359,6 +360,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
