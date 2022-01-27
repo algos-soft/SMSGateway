2a09e1af823fc99276f837ffc1d481ec8d685977
algos-soft <algos12>
2022-01-27 12:39:06 +0100

    icon fixed

--- app/src/main/java/it/algos/smsgateway/Constants.java
+++ app/src/main/java/it/algos/smsgateway/Constants.java
@@ -3,7 +3,7 @@ package it.algos.smsgateway;
 public class Constants {
 
     public static final String LOG_TAG="SMSGateway";
-    public static final int MAX_LOG_ITEMS=100;
+    public static final int MAX_LOG_ITEMS=1000;
 
 
 }
--- app/src/main/java/it/algos/smsgateway/MainActivity.java
+++ app/src/main/java/it/algos/smsgateway/MainActivity.java
@@ -1,11 +1,17 @@
 package it.algos.smsgateway;
 
 import android.Manifest;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -71,10 +77,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkPermissions(){
 
+        // check permission to send SMS
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
             String[] permissionsArray=new String[]{Manifest.permission.SEND_SMS};
             requestPermissions(permissionsArray, 3);
         }
+
+//        ActivityManager activitymanager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+//        boolean restricted=activitymanager.isBackgroundRestricted();
+
+        // check permission to ignore battery optimization
+        String packageName = getPackageName();
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + packageName));
+            startActivity(intent);
+        }
+
 
     }
 
